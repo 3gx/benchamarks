7881bf24410506e8b8a34c01b213b887c255a663
egaburov <egaburov.work@gmail.com>
2012-10-19 08:49:55 +0200

    +1

--- CUDA/concurrency/concurrency.cu
+++ CUDA/concurrency/concurrency.cu
@@ -8,51 +8,69 @@
 #define __out
 
 #define NTHREADS_MAX 1024
+#define WARP_SIZE 32
+
+#define ILP 8
+
+static __constant__ int warpMap[WARP_SIZE];
+
 
 template<typename REAL>
 __global__ void dev_compute(
+    const int nwarps,
     const int   nloop,
     __out REAL *in_a,
     const REAL *in_b,
     const REAL *in_c)
 {
   const int tid = threadIdx.x;
+  const int warpId = warpMap[tid / WARP_SIZE];
+  if (warpId >= nwarps) return;
 
-  REAL       a = in_a[tid];
-  const REAL b = in_b[tid];
-  const REAL c = in_c[tid];
+  REAL       a[ILP] = {in_a[tid]};
+  const REAL b      =  in_b[tid];
+  const REAL c      =  in_c[tid];
+  
+#pragma unroll
+  for (int i = 0; i < ILP; i++)
+    a[i] += (float)i*a[i];
 
-#pragma unroll 32
+#pragma unroll 64
   for (int i = 0; i < nloop; i++)
-    a += b*c;
+  {
+#pragma unroll
+    for (int j = 0; j < ILP; j++)
+      a[j] += b*c;
+  }
 
   /* unlikely it will ever write result to RAM */
-  if (a == REAL(123.123456))
-    in_a[tid] = a;
+  REAL asum = a[0];
+#pragma unroll
+  for (int j = 1; j < ILP; j++)
+    asum += a[j];
+  if (asum == REAL(123.123456))
+    in_a[tid] = asum;
 }
 
-template<typename T>
+  template<typename T>
 void run_test(const int nwarps, const int nblocks, const int n, const cuda_mem<T> &in, cuda_mem<T> &out)
 {
-  const int WARP_SIZE = 32;
-
   assert(nwarps > 0);
   assert(nblocks > 0);
 
   const dim3 grid(nblocks);
-  const dim3 blocks(WARP_SIZE * nwarps);
-  assert(blocks.x <= NTHREADS_MAX);
+  const dim3 blocks(NTHREADS_MAX);
 
   const double t0 = rtc();
 
-  dev_compute<T><<<grid, blocks>>>(n, out, in, in);
+  dev_compute<T><<<grid, blocks>>>(nwarps, n, out, in, in);
   CUDA_SAFE_CALL(cudaThreadSynchronize());
 
   const double t1 = rtc();
   const double dt = t1 - t0;
 
   fprintf(stderr, " nwarps= %d: done in %g sec perf= %g GFLOP/s\n", 
-      nwarps, dt, 2.0*grid.x*n*blocks.x/dt/1e9 );
+      nwarps, dt, ILP*2.0*grid.x*n*nwarps*WARP_SIZE/dt/1e9 );
 }
 
 
@@ -63,10 +81,34 @@ int main(int argc, char * argv[])
 
   const int nloop = nMloop * 1024;
 
+  const int warpStride = argc > 2 ? atoi(argv[2]) : 1;
+  fprintf(stderr, " testing warp-stride of %d \n", warpStride);
 
-  const int nblocks = 10240;
+  const int flag = argc > 3;
 
-  
+
+  static int warpMap_host[WARP_SIZE];
+  int warpId = 0;
+  int offset = 0;
+  fprintf(stderr, " Warp scheduling order:  \n  ");
+  for (int i = 0; i < WARP_SIZE; i++)
+  {
+    warpMap_host[i] = warpId + offset;
+    warpId += warpStride;
+    if (warpId >= WARP_SIZE)
+    {
+       offset += flag;
+       warpId -= WARP_SIZE;
+    }
+    fprintf(stderr, "%d ", warpMap_host[i]);
+  }
+  fprintf(stderr, " \n");
+  CUDA_SAFE_CALL(cudaMemcpyToSymbol("warpMap", warpMap_host, WARP_SIZE*sizeof(int)));
+
+
+  const int nblocks = 1;
+
+
   {
     fprintf(stderr, " --- fp32 ---  \n");
     cuda_mem<float> d_in, d_out;
@@ -75,12 +117,12 @@ int main(int argc, char * argv[])
     d_out.realloc(NTHREADS_MAX);
 
 
-    for (int i = 1; i <= 32; i <<= 1)
+    for (int i = 1; i <= NTHREADS_MAX/WARP_SIZE; i++)
     {
       run_test(i, nblocks, nloop, d_in, d_out);
     }
   }
-  
+
   {
     fprintf(stderr, " --- fp64 ---  \n");
     cuda_mem<double> d_in, d_out;
@@ -88,7 +130,7 @@ int main(int argc, char * argv[])
     d_in .realloc(NTHREADS_MAX);
     d_out.realloc(NTHREADS_MAX);
 
-    for (int i = 1; i <= 32; i <<= 1)
+    for (int i = 1; i <= NTHREADS_MAX/WARP_SIZE; i++)
     {
       run_test(i, nblocks, nloop, d_in, d_out);
     }
